c2065d5da3f25ff2d2b73dda8ca4a9415823f648
Emmanuel Jesus Estallo <erestallo@up.edu.ph>
2022-06-05 17:35:14 +0800

    updates

--- Signal Processing/sigproc.py
+++ Signal Processing/sigproc.py
@@ -2,13 +2,29 @@ import numpy as np
 from scipy import signal as sigproc 
 import pandas as pd 
 import matplotlib.pyplot as plt 
+import seaborn as sns 
 
+data = pd.read_csv('demod_out_A_normal_2.csv')
+
+time = data['Time'].to_numpy()
+val = data['Values'].to_numpy()
 
 fc1 = 10 
 fc2 = 450 
 fc3 = 20 
 
-butterHP_10Hz = sigproc.butter(4,10,'high', analog=False, fs=2000) 
-butterENVLP_450Hz = sigproc.butter(4,450,'low', analog=False, fs= 2000) 
-butterENVHP_20Hz = sigproc.butter(2,20,'high', analog=False, fs=2000)
+b10Hz, a10Hz = sigproc.butter(4,10,'high', analog=False, fs=2000) 
+b450Hz, a450Hz = sigproc.butter(4,450,'low', analog=False, fs= 2000) 
+b20Hz, a20Hz = sigproc.butter(2,20,'high', analog=False, fs=2000)
 
+sig1 = sigproc.filtfilt(b10Hz,a10Hz, val) 
+sig2 = sigproc.filtfilt(b450Hz,a450Hz, sig1) 
+sig_filt = sigproc.filtfilt(b20Hz,a20Hz,sig2) 
+
+rs = np.abs(sig_filt) 
+
+
+sns.set()
+sns.set_style("darkgrid")
+plt.plot(time,rs)
+plt.show()
